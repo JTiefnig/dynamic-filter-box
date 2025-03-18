dbc7d66aa52487e17454fd119d43157b05a5219a
Johannes Tiefnig <tiefnig@protonmail.com>
2025-03-19 00:44:37 +0100

    basic table filter for names

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import originaltabledata from "./data/data.json";
 
 function App() {
+  const [tabledata, setTableData] = useState(originaltabledata);
+
+  function setFilterData(e) {
+    const filterValue = e.target.value.toLowerCase();
+    const filteredData = originaltabledata.filter((item) =>
+      item.name.toLowerCase().includes(filterValue)
+    );
+    setTableData(filteredData);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome</h1>
+
+      <input
+        type="text"
+        placeholder="Filter by name"
+        onChange={setFilterData}
+      />
+
+      <table className="table">
+        <thead className="table-grid-header">
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody className="table-grid-item">
+          {tabledata.map((data, index) => (
+            <tr key={index}>
+              <td>{data.name}</td>
+              <td>{data.age}</td>
+              <td>{data.city}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
